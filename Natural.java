fda84c2bac51232e61ec2d4ae70b0848db2c7799
Darshanaborthakur <154210627+Darshanaborthakur@users.noreply.github.com>
2024-04-02 00:03:41 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,18 @@
+package Loop;
+
+import java.util.*;
+
+public class Natural {
+    
+     public static void main(String args[])
+     {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter  a number");
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++)
+        {
+            System.out.println(i);
+        }
+     }
+}
